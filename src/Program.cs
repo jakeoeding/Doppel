092904183fbfa15a69f726508df7abc3229c060a
jakeoeding <jaoeding@seas.upenn.edu>
2020-08-25 14:09:56 -0500

    created initial project structure

---
+++ src/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Doppel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
